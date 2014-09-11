61a83fd7b87d1317b527b886f8d2566e87a00040
shak <sovannara.hak@isir.upmc.fr>
2014-09-11 16:39:48 +0200

    Fix kinematic chain

--- src/kukakdl.cpp
+++ src/kukakdl.cpp
@@ -6,15 +6,60 @@
 #include "kukakdl.hpp"
 
 KukaKDL::KukaKDL(){
-    chain.addSegment(KDL::Segment("world",KDL::Joint(KDL::Joint::None),KDL::Frame(KDL::Rotation::RPY( 0,0,3.14159265359/6),KDL::Vector(0,  0, 0)))); 
-    chain.addSegment(KDL::Segment("world_robot",KDL::Joint("shoulder_0",KDL::Joint::RotZ),KDL::Frame(KDL::Rotation::RPY( 0,0,3.14159265359),KDL::Vector(0,  0, 0.3105)))); 
-    chain.addSegment(KDL::Segment("segment_1",KDL::Joint("shoulder_1",KDL::Joint::RotY),KDL::Frame(KDL::Rotation::RPY( 0,0,0),KDL::Vector(0,  0,  0))));
-    chain.addSegment(KDL::Segment("segment_2",KDL::Joint("shoulder_2",KDL::Joint::RotZ),KDL::Frame(KDL::Rotation::RPY(  0,0,3.14159265359),KDL::Vector(0, 0, 0.4))));
-    chain.addSegment(KDL::Segment("segment_3",KDL::Joint("elbow_0",KDL::Joint::RotY),KDL::Frame(KDL::Rotation::RPY(0, 0, 0),KDL::Vector(0, 0, 0))));
-    chain.addSegment(KDL::Segment("segment_4",KDL::Joint("wrist_0",KDL::Joint::RotZ),KDL::Frame(KDL::Rotation::RPY(0,  0, -1.570796326795),KDL::Vector(0, 0, 0.39))));
-    chain.addSegment(KDL::Segment("segment_5",KDL::Joint("wrist_1",KDL::Joint::RotX),KDL::Frame(KDL::Rotation::RPY(0, 0, 0),KDL::Vector(0,  0,  0))));
-    chain.addSegment(KDL::Segment("segment_6",KDL::Joint("wrist_2",KDL::Joint::RotZ),KDL::Frame(KDL::Rotation::RPY(0,  0,  0),KDL::Vector(0.016617,  -0.016932,  0.238283))));
-    chain.addSegment(KDL::Segment("segment_7",KDL::Joint(KDL::Joint::None),KDL::Frame(KDL::Rotation::RPY( 0,0,0),KDL::Vector(0,  0,  0))));
+    //joint 0
+    chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::None),
+                KDL::Frame::DH_Craig1989(0.0, 0.0, 0.31, 0.0)
+                ));
+    //joint 1
+    chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotZ),
+                KDL::Frame::DH_Craig1989(0.0, 1.5707963, 0.0, 0.0),
+                KDL::Frame::DH_Craig1989(0.0, 1.5707963, 0.0, 0.0).Inverse()*KDL::RigidBodyInertia(2,
+                    KDL::Vector::Zero(),
+                    KDL::RotationalInertia(0.0,0.0,0.0115343,0.0,0.0,0.0))));
+
+    //joint 2 
+    chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotZ),
+                KDL::Frame::DH_Craig1989(0.0, -1.5707963, 0.4, 0.0),
+                KDL::Frame::DH_Craig1989(0.0, -1.5707963, 0.4, 0.0).Inverse()*KDL::RigidBodyInertia(2,
+                    KDL::Vector(0.0,-0.3120511,-0.0038871),
+                    KDL::RotationalInertia(-0.5471572,-0.0000302,-0.5423253,0.0,0.0,0.0018828))));
+
+    //joint 3
+    chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotZ),
+                KDL::Frame::DH_Craig1989(0.0, -1.5707963, 0.0, 0.0),
+                KDL::Frame::DH_Craig1989(0.0, -1.5707963, 0.0, 0.0).Inverse()*KDL::RigidBodyInertia(2,
+                    KDL::Vector(0.0,-0.0015515,0.0),
+                    KDL::RotationalInertia(0.0063507,0.0,0.0107804,0.0,0.0,-0.0005147))));
+
+    //joint 4
+    chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotZ),
+                KDL::Frame::DH_Craig1989(0.0, 1.5707963, 0.39, 0.0),
+                KDL::Frame::DH_Craig1989(0.0, 1.5707963, 0.39, 0.0).Inverse()*KDL::RigidBodyInertia(2,
+                    KDL::Vector(0.0,0.5216809,0.0),
+                    KDL::RotationalInertia(-1.0436952,0.0,-1.0392780,0.0,0.0,0.0005324))));
+
+    //joint 5
+    chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotZ),
+                KDL::Frame::DH_Craig1989(0.0, 1.5707963, 0.0, 0.0),
+                KDL::Frame::DH_Craig1989(0.0, 1.5707963, 0.0, 0.0).Inverse()*KDL::RigidBodyInertia(2,
+                    KDL::Vector(0.0,0.0119891,0.0),
+                    KDL::RotationalInertia(0.0036654,0.0,0.0060429,0.0,0.0,0.0004226))));
+
+    //joint 6
+    chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotZ),
+                KDL::Frame::DH_Craig1989(0.0, -1.5707963, 0.0, 0.0),
+                KDL::Frame::DH_Craig1989(0.0, -1.5707963, 0.0, 0.0).Inverse()*KDL::RigidBodyInertia(2,
+                    KDL::Vector(0.0,0.0080787,0.0),
+                    KDL::RotationalInertia(0.0010431,0.0,0.0036376,0.0,0.0,0.0000101))));
+    //joint 7
+    chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotZ),
+                KDL::Frame::DH_Craig1989(0.0, 0.0, 0.078, 0.0),
+                KDL::Frame::DH_Craig1989(0.0, 0.0, 0.078, 0.0).Inverse()*KDL::RigidBodyInertia(2,
+                    KDL::Vector::Zero(),
+                    KDL::RotationalInertia(0.000001,0.0,0.0001203,0.0,0.0,0.0))));
+
+    //tool
+    chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::None), KDL::Frame::Identity()));
 
     fksolver = new KDL::ChainFkSolverPos_recursive(chain);
     jacsolver = new KDL::ChainJntToJacSolver(chain);
--- test/testkukakdl.cpp
+++ test/testkukakdl.cpp
@@ -5,15 +5,31 @@
 
 #include "kukakdl.hpp"
 #include <iostream>
+#include <kdl/frames_io.hpp>
 
 int main(){
     KukaKDL model;
-    std::vector<double> q(7);
-    for(int i=0; i<7;++i){
-        q[i] = i*0.2;
-    }
-    model.setJointPosition(q);
+    int segment = 8;
+    double q_1[] = {0.0, -0.0523626, 0.0, 1.51845, 0.0, -0.959863, 0.0};
+    double q_2[] = {0.829583, 0.118625, 0.0, 1.21193, -1.71438, -0.740072, 1.32882};
+    std::vector<double> q1(q_1, q_1+7);
+    std::vector<double> q2(q_2, q_2+7);
+    model.setJointPosition(q1);
     model.computeJacobian();
-    std::cout << model.jacobian.data << std::endl;
+    model.jacobian.changeBase(model.getSegmentPosition(segment).M.Inverse());
+
+    std::cout << model.getSegmentPosition(segment) << std::endl << std::endl;
+
+    std::cout << model.jacobian.data << std::endl <<std::endl;
+    
+    model.setJointPosition(q2);
+    model.computeJacobian();
+    model.jacobian.changeBase(model.getSegmentPosition(segment).M.Inverse()); 
+
+    std::cout << model.getSegmentPosition(segment) << std::endl << std::endl;
+
+    std::cout << model.jacobian.data << std::endl << std::endl;
+    
     return 0;
+
 }
