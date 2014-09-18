fc6fd33bd07a3e8dabdaa6801f45e981539a4537
Vincent Padois <vincent.padois@upmc.fr>
2014-09-18 15:39:22 +0200

    Bla

--- include/kukakdl/kukakdl.hpp
+++ include/kukakdl/kukakdl.hpp
@@ -10,21 +10,63 @@
 #include <kdl/chain.hpp>
 #include <kdl/chainfksolverpos_recursive.hpp>
 #include <kdl/chainjnttojacsolver.hpp>
+#include <kdl/chaindynparam.hpp>
+#include <kdl/jntspaceinertiamatrix.hpp>
 #include <Eigen/Dense>
+#include "kukakdl/kukakdl_const.hpp"
 
 class KukaKDL{
+	
     public:
         KukaKDL();
         KDL::Chain chain;
         KDL::Jacobian jacobian;
         KDL::JntArray q;
+        KDL::JntArray qd;
         KDL::ChainFkSolverPos_recursive* fksolver;
         KDL::ChainJntToJacSolver* jacsolver;
+        KDL::JntSpaceInertiaMatrix massMatrix;
+        KDL::JntArray corioCentriGravTorque;
+        KDL::JntArray frictionTorque;
+        KDL::JntArray gravityTorque;
 
         void computeJacobian();
         void setJointPosition(std::vector<double> &q_des);
-
+        void setJointVelocity(std::vector<double> &qd_des);
+        void setLastLinkWithToolInertia(KDL::RotationalInertia toolRInertia,KDL::Vector coordCOG,double m);
+        void setExternalToolWrench(KDL::Wrench W_ext);
+        void computeMassMatrix();
+        void computeCorioCentriGravTorque();
+        void computeGravityTorque();
+        void computeFrictionTorque();
         KDL::Frame getSegmentPosition(int segment);
+        
+        //  For testing purpose (to compare the dynamics models of the KUKA obtained from SYMORO+ and KDL)
+        KDL::ChainDynParam* dynModelSolver;
+        KDL::JntSpaceInertiaMatrix massMatrixFromKDL;
+        KDL::JntArray corioCentriTorqueFromKDL;
+        KDL::JntArray gravityTorqueFromKDL;
+        void computeMassMatrixFromKDL();
+		void computeCorioCentriTorqueFromKDL();
+		void computeGravityTorqueFromKDL();
+	
+	private:
+		// Last Link With Tool Inertia parameters
+		double XX7_tool;
+		double XY7_tool;
+		double XZ7_tool;
+		double YY7_tool;
+		double YZ7_tool;
+		double ZZ7_tool; 
+		
+		// Last Link Wrench (expressed in the last link frame)
+		double FX7;
+		double FY7;
+		double FZ7;
+		double CX7;
+		double CY7;
+		double CZ7; 
+		
 };
 
 #endif
--- test/testkukakdl.cpp
+++ test/testkukakdl.cpp
@@ -6,12 +6,15 @@
 #include "kukakdl/kukakdl.hpp"
 #include <iostream>
 #include <kdl/frames_io.hpp>
+#include <Eigen/Eigenvalues> 
+#include <Eigen/Dense>
 
 int main(){
     KukaKDL model;
     int segment = 8;
     double q_1[] = {0.0, -0.0523626, 0.0, 1.51845, 0.0, -0.959863, 0.0};
-    double q_2[] = {0.829583, 0.118625, 0.0, 1.21193, -1.71438, -0.740072, 1.32882};
+    //double q_2[] = {0.829583, 0.118625, 0.0, 1.21193, -1.71438, -0.740072, 1.32882};
+    double q_2[] = {0.,0.,0.,0.,0.,0.,0.};
     std::vector<double> q1(q_1, q_1+7);
     std::vector<double> q2(q_2, q_2+7);
     model.setJointPosition(q1);
@@ -23,6 +26,7 @@ int main(){
     std::cout << model.jacobian.data << std::endl <<std::endl;
     
     model.setJointPosition(q2);
+    model.setJointVelocity(q2);
     model.computeJacobian();
     model.jacobian.changeBase(model.getSegmentPosition(segment).M.Inverse()); 
 
@@ -33,8 +37,37 @@ int main(){
     std::cout << "Change ref point " << std::endl;
     KDL::Vector v(0.0, -0.3, 0.2);
     model.jacobian.changeRefPoint(v); 
-    std::cout << model.jacobian.data << std::endl;
+    std::cout << model.jacobian.data << std::endl << std::endl;
 
+	std::cout << "Mass Matrix from SYMORO+ " << std::endl << std::endl;
+	model.computeMassMatrix();
+	std::cout << model.massMatrix.data << std::endl << std::endl;
+	std::cout << "Mass Matrix from SYMORO+ : symmetry test " << std::endl << std::endl;
+	std::cout << model.massMatrix.data - model.massMatrix.data.transpose() << std::endl << std::endl;
+	std::cout << "Mass Matrix from SYMORO+ : >0 test "<< std::endl << std::endl;
+	Eigen::EigenSolver<Eigen::MatrixXd> es(model.massMatrix.data);
+	std::cout << "The eigenvalues of the matrix are:" << std::endl << es.eigenvalues() << std::endl << std::endl;
+    
+    std::cout << "Mass Matrix from SYMORO+ vs from KDL " << std::endl << std::endl;
+    std::cout << model.massMatrix.data << std::endl << std::endl;
+    model.computeMassMatrixFromKDL();
+    std::cout << model.massMatrixFromKDL.data << std::endl << std::endl;
+    
+    std::cout << "Gravity vector from SYMORO+ vs from KDL " << std::endl << std::endl;
+    model.computeGravityTorque();
+    std::cout << model.gravityTorque.data << std::endl << std::endl;
+    model.computeGravityTorqueFromKDL();
+    std::cout << model.gravityTorqueFromKDL.data << std::endl << std::endl;
+    
+    std::cout << "CoriofugeFric vector from SYMORO+ vs from KDL " << std::endl << std::endl;
+    model.computeCorioCentriGravTorque();
+    std::cout << model.corioCentriGravTorque.data - model.gravityTorque.data << std::endl << std::endl;
+    model.computeCorioCentriTorqueFromKDL();
+    std::cout << model.corioCentriTorqueFromKDL.data << std::endl << std::endl;
+    
+    std::cout << "Fric vector from SYMORO+ " << std::endl << std::endl;
+    model.computeFrictionTorque();
+    std::cout << model.frictionTorque.data << std::endl << std::endl;
     
     return 0;
 
